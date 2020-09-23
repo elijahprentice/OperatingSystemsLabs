a853545247a5fee90fd62249383d305c81fc7c51
Elijah Prentice <prentee1213@gmail.com>
2020-09-23 17:05:54 -0400

    Update process_intro.c

--- Lab 2 - intro to processes/process_intro.c
+++ Lab 2 - intro to processes/process_intro.c
@@ -1,21 +1,22 @@
 #include  <stdio.h>
 #include  <stdlib.h>
 #include  <sys/types.h>
-#include  <time.h>
 
 #define   MAX_COUNT  30
 
 void  ChildProcess(void);                /* child process prototype  */
 void  ParentProcess(void);               /* parent process prototype */
 
+int pids[2];
+
 void  main(void)
 {
-     pid_t  pid;
 
      //create the first child
-     pid = fork();
+     pids[0] = fork();
+     srand(getpid());
      
-     if (pid == 0){
+     if (pids[0] == 0){
        
        //call ChildProcess() for the first child
        ChildProcess();
@@ -23,9 +24,9 @@ void  main(void)
      } else {
        
        //create the second child
-       pid = fork();
+       pids[1] = fork();
        
-       if (pid == 0){
+       if (pids[0] == 0){
          
          //call ChildProcess() for the second child
          ChildProcess();
@@ -41,11 +42,9 @@ void  main(void)
 void  ChildProcess(void)
 {
   
-  srand(time(0));      //seed random based on time
-  
   //the child goes to sleep for a random number of times between 1 and 30
   int i;
-  for (i = (rand() % 30 + 1); i > 0; i--){
+  for (i > 0; i < (rand() % MAX_COUNT + 1); i--){
     
     //the child sleeps for a random amout of time between 1 and 10 seconds
     printf("Child Pid: %d is going to sleep!\n", getpid());
@@ -63,9 +62,9 @@ void  ParentProcess(void)
   
   //parent waits for the first child process to complete
   wait(NULL);
-  printf("Child Pid: %d has completed.\n", getpid());
+  printf("Child Pid: %d has completed.\n", pids[0]);
   
   //parent waits for the second child process to complete
   wait(NULL);
-  printf("Child Pid: %d has completed.\n", getpid());
+  printf("Child Pid: %d has completed.\n", pids[1]);
 }
